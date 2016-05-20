6a1ae10d8e0b25fbe09925b6fd61c6d68adc0277
Ben <BenElla91@gmail.com>
2016-05-20 04:11:11 +0300

    ben's work

--- app/src/main/java/com/example/yasmindunsky/playground/MainActivity.java
+++ app/src/main/java/com/example/yasmindunsky/playground/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.yasmindunsky.playground;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView line = (TextView) findViewById(R.id.Chooseline);
+        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/Montserrat-Bold.ttf");
+        line.setTypeface(face);
     }
 
     public void getSelection(View view){
         int buttonId = view.getId();
         Intent intent = new Intent(this, MainActivity.class);;
+
+
         switch (buttonId){
             case R.id.run:
             {
@@ -32,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
         }
+
         startActivity(intent);
     }
+
+
 }
